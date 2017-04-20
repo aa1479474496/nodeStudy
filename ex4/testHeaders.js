e673640cd716745efd7b248078b6ec49c33bdfa9
aa1479474496 <1479474496@qq.com>
2017-04-20 17:42:22 +0800

    打印头部信息

---
+++ ex4/testHeaders.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/headers', function(req, res) {
+    res.set('Content-Type', 'text/plain');
+    var s = '';
+    
+    for (var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
+    // console.log(s);
+    res.send(s);
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + app.get('port'));
+});
