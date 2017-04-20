2507b02c623cd7cf623e8d91052ec0ef83ec4552
aa1479474496 <1479474496@qq.com>
2017-04-20 10:10:36 +0800

    ex2 handlebars模板的使用

--- ex2/meadowlark.js
+++ ex2/meadowlark.js
@@ -2,23 +2,33 @@ var express = require('express');
 
 var app = express();
 
+//设置handlebars视图引擎
+var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
-//定制404界面
-app.use(function (req, res) {
-    res.type('text/plain');
+app.get('/', function(req, res) {
+    res.render('home');
+});
+app.get('/about', function(req, res) {
+    res.render('about')
+});
+
+//404 catch-all 处理器(中间件)
+app.use(function(req, res) {
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
-//定制500页面
-app.use(function (err, req, res, next) {
+//500错误处理器(中间件)
+app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
-app.listen(app.get('port'), function () {
+app.listen(app.get('port'), function() {
     console.log('Express started on http://localhost:' + app.get('port'));
 });
